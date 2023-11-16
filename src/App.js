127071fdc0f247b069b9f9601de3d627547d75a6
lkrusso <lararusso.uk@gmail.com>
2023-11-16 13:35:29 +0000

    feat: Create RecipeCard component with hard-coded values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Route, Router, BrowserRouter } from "react-router-dom";
-import "";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import RecipeCard from "./components/RecipeCard/RecipeCard";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route />
+          <Route path="/" element={<RecipeCard />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/RecipeCard/RecipeCard.js
@@ -0,0 +1,42 @@
+import "./RecipeCard.scss";
+
+function RecipeCard({ recipe }) {
+  return (
+    <article className="recipe">
+      <h3 className="recipe__name">Recipe name</h3>
+      <img
+        src="https://placehold.co/75x25"
+        alt="placeholder image"
+        className="recipe__image"
+      />
+      <section className="recipe__details">
+        <p className="recipe__duration">Duration:</p>
+        <p className="recipe__serve">Serves:</p>
+        <p className="recipe__type">Type: </p>
+        <div className="recipe__further-details">
+          <ul className="recipe__ingredients">
+            {" "}
+            Ingredients:
+            <li>Ingredient one</li>
+            <li>Ingredient two</li>
+          </ul>
+          <ol className="recipe__instructions">
+            {" "}
+            Instructions:
+            <li>Instruction one</li>
+            <li>Instruction two</li>
+          </ol>
+        </div>
+      </section>
+      <section className="recipe__info">
+        <p className="recipe__review">Review:</p>
+        <div className="recipe__rating">Rating:</div>
+        <p className="recipe__comments">
+          Further comments about the recipe such as changes or thoughts
+        </p>
+      </section>
+    </article>
+  );
+}
+
+export default RecipeCard;
