1da70608a5c18ca484a77fa41417bc6bc54d48bf
lkrusso <lararusso.uk@gmail.com>
2023-11-20 15:10:13 +0000

    feat: Get list of ingredients incl. name and quantity via POST request to API

--- src/components/RecipeIngredients/RecipeIngredients.js
+++ src/components/RecipeIngredients/RecipeIngredients.js
@@ -1,11 +1,44 @@
 import "./RecipeIngredients.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function RecipeIngredients({ ingredients }) {
+function RecipeIngredients({ recipeID }) {
+  const [isError, setIsError] = useState(false);
+  const [ingredients, setIngredients] = useState([]);
+  useEffect(() => {
+    const getIngredients = async () => {
+      try {
+        const { data } = await axios.post(
+          "http://localhost:5050/api/ingredients",
+          {
+            id: recipeID,
+          }
+        );
+        console.log(data);
+        setIngredients(data);
+        setIsError(false);
+      } catch (error) {
+        setIsError(true);
+      }
+    };
+    getIngredients();
+  }, []);
+
+  if (isError) {
+    return <p>Error</p>;
+  }
+  if (!ingredients) {
+    return <p>Loading...</p>;
+  }
   return (
     <ul className="recipe__ingredients">
       Ingredients:
       {ingredients.map((ingredient) => {
-        return <li className="recipe__ingredient">{ingredient}</li>;
+        return (
+          <li className="recipe__ingredient">
+            {ingredient.quantity} {ingredient.name}
+          </li>
+        );
       })}
     </ul>
   );
