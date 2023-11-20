54a47fa75f81dfaf7d2d60dfbfba7d1ce9588580
lkrusso <lararusso.uk@gmail.com>
2023-11-20 15:30:34 +0000

    feat: Create Cookbook component

---
+++ src/components/Cookbook/Cookbook.js
@@ -0,0 +1,11 @@
+import "./Cookbook.scss";
+
+function Cookbook() {
+  return (
+    <article className="cookbook">
+      <h3 className="cookbook__title">Cookbook title</h3>
+    </article>
+  );
+}
+
+export default Cookbook;
