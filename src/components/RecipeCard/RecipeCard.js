72ee50e4c44b48016c34e6e0507c8a60a45935ec
lkrusso <lararusso.uk@gmail.com>
2023-11-20 09:16:29 +0000

    fix: Refactor RecipeCard component to no longer use hard-coded values

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,39 +1,27 @@
 import RecipeInstructions from "../RecipeInstructions/RecipeInstructions";
 import RecipeIngredients from "../RecipeIngredients/RecipeIngredients";
 import "./RecipeCard.scss";
+import { useEffect } from "react";
 
 function RecipeCard({ recipe }) {
+  useEffect(async () => {}, []);
   return (
     <article className="recipe">
-      <h3 className="recipe__name">Recipe name</h3>
+      <h3 className="recipe__name">{recipe.title}</h3>
       <img
-        src="https://placehold.co/75x25"
-        alt="placeholder image"
+        src={recipe.image_url ? recipe.image_url : "https://placehold.co/75x25"}
+        alt={
+          recipe.image_url ? `image of ${recipe.title}` : "placeholder image"
+        }
         className="recipe__image"
       />
       <section className="recipe__details">
-        <p className="recipe__duration">Duration:</p>
-        <p className="recipe__serve">Serves:</p>
-        <p className="recipe__type">Type: </p>
+        <p className="recipe__duration">Duration: {recipe.duration}</p>
+        <p className="recipe__serve">Serves: {recipe.serves}</p>
+        <p className="recipe__type">Type: {recipe.cuisine_type}</p>
         <div className="recipe__further-details">
-          <ul className="recipe__ingredients">
-            {" "}
-            Ingredients:
-            <li>Ingredient one</li>
-            <li>Ingredient two</li>
-          </ul>
-          <RecipeInstructions
-            instructions={["Instruction one", "Instruction two"]}
-          />
-          <RecipeIngredients
-            ingredients={["Ingredient one", "Ingredient two"]}
-          />
-          <ol className="recipe__instructions">
-            {" "}
-            Instructions:
-            <li>Instruction one</li>
-            <li>Instruction two</li>
-          </ol>
+          <RecipeIngredients ingredients={recipe.ingredients} />
+          <RecipeInstructions instructions={recipe.instructions} />
         </div>
       </section>
       <section className="recipe__info">
